d9ac9c61bd6d9243c2d2de5c331035b5e27f5b16
petriwna <petriwna43@gmail.com>
2023-05-24 14:51:32 +0300

    (feat) calculation

--- src/logic/Calculator.ts
+++ src/logic/Calculator.ts
@@ -1,18 +1,19 @@
 import { Button } from './Button';
+import { Calculation } from './Calculation';
 import { ICalculator } from './ICalculator';
 
 export class Calculator implements ICalculator {
   displayValue: string;
-  firstOperand: number | null;
+  firstOperand: number;
   waitingForSecondOperand: boolean;
-  operator: string | null;
+  operator: string;
   member: number;
 
   constructor() {
     this.displayValue = '0';
-    this.firstOperand = null;
+    this.firstOperand = 0;
     this.waitingForSecondOperand = false;
-    this.operator = null;
+    this.operator = '';
     this.member = 0;
   }
 
@@ -29,6 +30,9 @@ export class Calculator implements ICalculator {
       case 'number':
         this.handleNumber(btn.value);
         break;
+      case 'action':
+        console.log('open');
+        break;
     }
   }
 
@@ -39,20 +43,58 @@ export class Calculator implements ICalculator {
       case '*':
       case '/':
       case '=':
-        console.log(this.displayValue);
+        this.checkOperator(operator);
+        break;
+      case '+/-':
+        this.inputNegativeNumber();
         break;
       case '.':
-        this.inputDecimal(operator);
+        this.inputDecimal();
         break;
       case 'clear':
         this.clearAll();
         break;
       case 'backspace':
-        this.handleBackspace(this.displayValue);
+        this.handleBackspace();
         break;
     }
     this.updateDisplay(this.displayValue);
+  }
+
+  checkOperator(operator: string) {
+    const inputValue = parseFloat(this.displayValue);
+    if (this.operator && this.waitingForSecondOperand) {
+      this.operator = operator;
+      return;
+    }
+
+    if (this.firstOperand === null && !isNaN(inputValue)) {
+      this.firstOperand = inputValue;
+    } else if (this.operator) {
+      const result = this.compute(this.firstOperand, inputValue, this.operator);
+      this.displayValue = `${parseFloat(result.toFixed(7))}`;
+      this.firstOperand = result;
+    }
+
     this.waitingForSecondOperand = true;
+    this.operator = operator;
+  }
+
+  compute(firstOperand: number, secondOperand: number, operator: string) {
+    const calculation = new Calculation();
+
+    switch (operator) {
+      case '+':
+        return calculation.add(firstOperand, secondOperand);
+      case '-':
+        return calculation.divide(firstOperand, secondOperand);
+      case '*':
+        return calculation.multiply(firstOperand, secondOperand);
+      case '/':
+        return calculation.subtract(firstOperand, secondOperand);
+      default:
+        return secondOperand;
+    }
   }
 
   handleNumber(value: string): void {
@@ -65,17 +107,39 @@ export class Calculator implements ICalculator {
     this.updateDisplay(this.displayValue);
   }
 
-  handlerFn(): void {}
+  handlerFn(): void {
+    //TODO
+  }
 
-  handlerMemory(): void {}
+  handlerMemory(): void {
+    //TODO
+  }
 
-  inputDecimal(dot: string): void {}
+  inputNegativeNumber(): void {
+    const currentInput = parseFloat(this.displayValue);
 
-  inputDisplay(): void {}
+    const result = currentInput * -1;
+    this.displayValue = `${parseFloat(result.toFixed(10))}`;
 
-  handleBackspace(value: string): void {
-    if (value.length > 1) {
-      this.displayValue = value.slice(0, -1);
+    if (this.waitingForSecondOperand) {
+      this.waitingForSecondOperand = false;
+    }
+  }
+
+  inputDecimal(): void {
+    if (this.waitingForSecondOperand) {
+      this.displayValue = '0';
+      this.waitingForSecondOperand = false;
+      return;
+    }
+    if (!this.displayValue.includes('.')) {
+      this.displayValue += '.';
+    }
+  }
+
+  handleBackspace(): void {
+    if (this.displayValue.length > 1) {
+      this.displayValue = this.displayValue.slice(0, -1);
     } else {
       this.displayValue = '0';
     }
@@ -83,9 +147,9 @@ export class Calculator implements ICalculator {
 
   clearAll(): void {
     this.displayValue = '0';
-    this.firstOperand = null;
+    this.firstOperand = 0;
     this.waitingForSecondOperand = false;
-    this.operator = null;
+    this.operator = '';
     this.updateDisplay(this.displayValue);
   }
 }
--- src/logic/ICalculator.ts
+++ src/logic/ICalculator.ts
@@ -1,11 +1,13 @@
 export interface ICalculator {
-  inputDisplay: () => void;
   updateDisplay: (displayValue: string) => void;
   handlerButtons: (button: any) => void;
   handleOperator: (value: string) => void;
   handleNumber: (value: string) => void;
   handlerFn: () => void;
-  inputDecimal: (value: string) => void;
+  checkOperator: (operator: string) => void;
+  compute: (firstOperand: number, secondOperand: number, operator: string) => number;
+  inputDecimal: () => void;
+  inputNegativeNumber: () => void;
   handlerMemory: () => void;
   handleBackspace: (value: string) => void;
   clearAll: () => void;
